37da6cd4e32f26acfa9bf2dbdc04849ba98265e5
rossum <rossum@tuta.io>
2020-09-27 17:43:15 +0200

    membership page style improve

--- src/templates/membership-page.js
+++ src/templates/membership-page.js
@@ -36,14 +36,14 @@ export const MembershipPageTemplate = ({ goals, hero, membership, title }) => {
             <p className="text-lg">HDIR danas ima oko 10.000 članova iz preko 90 zemalja iz cijelog svijeta. Pridruži se i postani članom EACR društva.</p>
             <p className="font-extrabold text-2xl md:text-4xl pt-16">Cijena članarine</p>
             <div className="sm:flex pt-16">
-                <div className="bg-white p-8 rounded-lg shadow w-64 flex flex-col">
+                <div className="bg-white h-64 p-8 rounded-lg shadow w-64 flex flex-col">
                     <p className="font-extrabold text-xl flex-grow">Student</p>
-                    <p className="text-3xl text-center mt-16">Bez naknade</p>
-                    <p className="opacity-50 text-sm text-center">Svi studenti dodiplomskih, diplomskih i doktorskij studija te nezaposleni.</p>
+                    <p className="text-3xl font-semibold text-center mt-16">Bez naknade</p>
+                    <p className="opacity-50 text-sm text-center">Svi studenti dodiplomskih, diplomskih i doktorskij studija te nezaposleni</p>
                 </div>
-                <div className="bg-white mt-8 sm:mt-0 sm:ml-12 p-8 rounded-lg shadow w-64">
+                <div className="bg-white h-64 p-8 rounded-lg shadow w-64 flex flex-col sm:ml-12 mt-8 sm:mt-0">
                     <p className="font-extrabold text-xl flex-grow">Član</p>
-                    <p className="text-5xl text-center mt-16 leading-none">20€</p>
+                    <p className="text-5xl font-medium text-center mt-16 leading-none">20€</p>
                     <p className="opacity-50 text-center">~151,00 kn godišnje</p>
                 </div>
             </div>
@@ -51,12 +51,46 @@ export const MembershipPageTemplate = ({ goals, hero, membership, title }) => {
       </section>
       <section className="bg-gray-200 py-24">
         <div className="limit text-gray-600">
+            <p className="font-extrabold text-2xl md:text-4xl">Novi članovi</p>
+            <p className="mt-6 text-lg">Prijava za članstvo se izvodi kroz online obrazac.</p>
+            <Link className="btn mt-6" to="/">Učlani se</Link>
+        </div>
+      </section>
+      <section className="bg-gray-200 py-24">
+        <div className="limit text-gray-600">
             <p className="font-extrabold text-2xl md:text-4xl">Podaci za plaćanje</p>
-            <ul className="mt-12 text-lg">
-                <li>Primatelj: <span className="font-semibold">HDIR, Bijenička 54, 10000 Zagreb</span></li>
-                <li>IBAN primatelja: <span className="font-number font-medium">HR5023600001102084564</span></li>
-                <li>Opis plaćanja: <span className="font-semibold">Vaše ime i prezime – članarina za 2020.</span></li>
-            </ul>
+            <div className="max-w-lg mt-6">
+                <div class="sm:flex sm:items-center mb-6">
+                    <div class="sm:w-1/3">
+                        <label class="block text-gray-600 font-bold sm:text-right mb-1 sm:mb-0 pr-4" for="inline-full-name">
+                            Primatelj
+                        </label>
+                    </div>
+                    <div class="sm:w-2/3">
+                        <input class="overflow-hidden truncate bg-white appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none disabled" type="text" value="HDIR, Bijenička 54, 10000 Zagreb" />
+                    </div>
+                </div>
+                <div class="sm:flex sm:items-center mb-6">
+                    <div class="sm:w-1/3">
+                        <label class="block text-gray-600 font-bold sm:text-right mb-1 sm:mb-0 pr-4" for="inline-full-name">
+                            IBAN primatelja
+                        </label>
+                    </div>
+                    <div class="sm:w-2/3">
+                        <input class="overflow-hidden truncate bg-white appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none disabled font-number font-light" type="text" value="HR5023600001102084564" />
+                    </div>
+                </div>
+                <div class="sm:flex sm:items-center mb-6">
+                    <div class="sm:w-1/3">
+                        <label class="block text-gray-600 font-bold sm:text-right mb-1 sm:mb-0 pr-4" for="inline-full-name">
+                            Opis plaćanja
+                        </label>
+                    </div>
+                    <div class="sm:w-2/3">
+                        <input class="overflow-hidden truncate bg-white appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none disabled" type="text" value="Ime i Prezime – članarina za 2020." />
+                    </div>
+                </div>
+            </div>
         </div>
       </section>
     </div>
